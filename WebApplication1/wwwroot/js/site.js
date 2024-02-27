3f3e7a3685ab58df6c49111ce3ff2d0bb3928496
vlad <vlandgor@gmail.com>
2024-02-27 13:06:46 -0500

    moved scripts to the site.js

--- WebApplication1/wwwroot/js/site.js
+++ WebApplication1/wwwroot/js/site.js
@@ -1,4 +1,18 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
-// Write your JavaScript code.
+$(document).ready(function() {
+    // When the button is clicked
+    $(".project-button").click(function() {
+        // Toggle the visibility of the hidden menu
+        $("#projectInfoPanel").toggle();
+
+        // Update the text content of the hidden menu with the project name
+        var projectFullName = $(this).data('fullname');
+        var projectShortName = $(this).data('shortname')
+        var projectId = $(this).data('project-id');
+        $("#projectName").text(projectFullName);
+
+        $("#sessionLink").attr('href', '/Session?projectId=' + projectId);
+    });
+});
