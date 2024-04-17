a34b67ba24462e1b8220c186e85a94f56bb51a29
vlad <vlandgor@gmail.com>
2024-04-17 17:24:33 -0400

    Small visual update

--- WebApplication1/obj/Debug/net7.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net7.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea15beddf40058aab00c6017be737cf29949d2ba")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+983569cbbe48f43d93145b4577243caf68a33684")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
