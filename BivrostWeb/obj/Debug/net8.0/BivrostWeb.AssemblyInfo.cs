5362bd2ac45da13048aa6d84e2dcd15df23361dc
vlad <vlandgor@gmail.com>
2024-06-10 12:41:50 -0400

    Fixed side menu position

--- BivrostWeb/obj/Debug/net8.0/BivrostWeb.AssemblyInfo.cs
+++ BivrostWeb/obj/Debug/net8.0/BivrostWeb.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BivrostWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd29a1402ceb1edde2101464dd6a14a56d0be3d6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3d40860e3aefca0fb04e36ba237c4b155dc052d")]
 [assembly: System.Reflection.AssemblyProductAttribute("BivrostWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BivrostWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
